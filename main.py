bb5f090d19c92c4966f98108264639a32dae5cb1
Miller Hooks <145886322+DickyMirrors@users.noreply.github.com>
2023-09-24 19:14:34 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+import pysubs2
+from morse import encrypt, decrypt
+import ffmpeg
+
+# pysub2 for transcript parsing
+# https://pysubs2.readthedocs.io/en/latest/index.html
+def subload():
+  subs = pysubs2.load("my_subtitles.ass", encoding="utf-8")
+  subs.shift(s=2.5)
+  for line in subs:
+      line.text = "{\\be1}" + line.text
+  subs.save("my_subtitles_edited.ass")
+
+# ffmpeg concat
+# https://www.reddit.com/r/learnpython/comments/rbr32l/combine_list_of_video_into_single_video/ilre0ia/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+def video_stitch():
+  # list of video files
+  files = []
+
+  # eparate video and audio, then flat the array
+  video_and_audios_files = [item for sublist in map(lambda f: [f.video, f.audio], files) for item in sublist]
+
+  # concat all
+  joined = (
+      ffmpeg
+        .concat(*video_and_audios_files, v=1, a=1)
+        .node
+    )
+
+  # merge video and audio
+  (
+    ffmpeg
+      .output(joined[0], joined[1], 'out.mp4')
+      .run()
+  )
+  
+# Text morse code converter
+# https://www.geeksforgeeks.org/morse-code-translator-python/
+# Hard-coded driver function to run the program
+def main():
+    message = "GEEKS-FOR-GEEKS"
+    result = encrypt(message.upper())
+    print (result)
+ 
+    message = "--. . . -.- ... -....- ..-. --- .-. -....- --. . . -.- ... "
+    result = decrypt(message)
+    print (result)
+ 
+# Executes the main function
+if __name__ == '__main__':
+    main()
